0455707dd7cc8ad32ef390cc4ff0faf7b08dd73f
Javier Ramirez <jramir11@bancoestado.cl>
2022-10-27 16:44:39 -0300

    se incluye funcion Math

--- monticuloMinimoIns.js
+++ monticuloMinimoIns.js
@@ -4,26 +4,28 @@ function insertIntoHeap(heap, val){
     let indice_padre=0;
     let temp=0;
 
-        //comprobando si esta vacio
+    //comprobando si esta vacio
     if (heap.length==0){
         heap.push(undefined);
     }
+
     //insertando el nuevo valor en el array heap
     heap.push(val);
+
     //indice del nuevo elemento
     indice_hijo = heap.length - 1;
+    indice_padre = Math.trunc(indice_hijo / 2);
 
     //compara padre e hijo e invierte valores si padre es mayor
-    while (heap[indice_hijo - 1] > heap[indice_hijo] && indice_hijo - 1 > 0){
+    while (heap[indice_padre] > heap[indice_hijo]){
         temp = heap[indice_hijo];
-        heap[indice_hijo] = heap[indice_hijo - 1];
-        heap[indice_hijo - 1] = temp;
+        heap[indice_hijo] = heap[indice_padre];
+        heap[indice_padre] = temp;
 
-        indice_hijo=indice_hijo - 1;
+        indice_hijo=indice_padre;
+        indice_padre = Math.trunc(indice_hijo / 2);
     }
     //muestra array heap
-    for (i=0;i<heap.length;i++){
-        console.log("indice=" + i + " valor=" + heap[i]);
-    }
+    console.log(heap);
 }
-insertIntoHeap([undefined,23,5], 13);  
+insertIntoHeap([undefined, 3, 8, 10, 11, 9, 20, 14], 7); // res= [undefined, 3, 7, 10, 8, 9, 20, 14, 11]
